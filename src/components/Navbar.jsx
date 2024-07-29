ddb151e8428839e237032aae76ca6493f42b14e2
Rahi Reyaz <rahiryazdar@gmail.com>
2024-07-29 23:21:18 +0530

    minor fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 
-import logo from '../assets/Logo.png';
+import logo from "../assets/Logo.png";
 import { FaLinkedin, FaGithub, FaPhoneSquare } from 'react-icons/fa';
 import { BiMailSend } from 'react-icons/bi';
 import {CONTACT} from "../assets/Info"
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className="flex items-center space-x-4 text-xl lg:text-2xl">
         {/* LinkedIn */}
         <a
-          href="https://www.linkedin.com/in/rahi-reyaz"
+          href={CONTACT.linkedin}
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-red-700"
